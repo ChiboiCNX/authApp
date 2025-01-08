813a79dcbcd16c8fb0eaf3b1114eeb93e11516a5
Chibioi <ezehstanley2002@gmail.com>
2025-01-08 15:49:14 +0100

    updated

--- authapp/urls.py
+++ authapp/urls.py
@@ -5,7 +5,6 @@ from . import views
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('signin/', views.signin, name='signin'),
-    path('success/', views.success, name='success'),
     path('home/', views.home, name='home'),
     path('logout/', views.logout, name='logout'),
 
--- authapp/views.py
+++ authapp/views.py
@@ -1,24 +1,22 @@
-from django.shortcuts import render,redirect
-# from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 from .forms import CreateUserForm
-from django.contrib.auth.models import User
+
 
 def signup(request):
     form = CreateUserForm()
+
     if request.method == 'POST':
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('success')
+            print('User created')  # Replace with logging for production
+            return redirect("signin")
         else:
-            return render(request, 'signup.html', {'form': form})
-        
-    return render(request, 'signup.html')    
+            print('Form errors:', form.errors)  # Debugging: Log form errors
+
+    context = {'form': form}
+    return render(request, 'signup.html', context)
        
-
-    
-      
-
 def signin(request):
     return render(request, 'signin.html')
 
